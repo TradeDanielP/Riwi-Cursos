041708d8851bd66f8cfc51194411ebac5b549c65
TradeDanielP <tradedanielp@gmail.com>
2024-05-27 13:43:52 -0500

    Spring Initializr app, database connection, add dependency Validation

---
+++ src/test/java/com/riwi/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
